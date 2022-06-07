03d5839618244b43fc7b4a939c1b99e04e3e3a48
Misael Correia <99922563+misaelccorreia@users.noreply.github.com>
2022-06-07 13:21:39 -0300

    It's a the first upload
    My english is very dumb, but is with i have in this moment. In this moment.

---
+++ main.js
@@ -0,0 +1,30 @@
+//string(textos)
+// Number (numero)
+// bollean (true | false)
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, #
+services,
+#services header,
+#services .card`)
+
+//Estou no minuto 1:10:00
